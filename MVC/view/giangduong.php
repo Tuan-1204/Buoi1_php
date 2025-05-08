89a76041d5754635598c7160a7d93328c4689eb5
Nguyen Anh Tuan <Nganhtuan5847@gmail.com>
2025-05-08 16:28:12 +0700

    fixx tiêu đề

--- MVC/view/giangduong.php
+++ MVC/view/giangduong.php
@@ -2,7 +2,7 @@
 <html lang="vi">
 <head>
     <meta charset="UTF-8">
-    <title>Đăng ký thời khóa biểu</title>
+    <title>Đăng ký Giảng Đường</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <style>
         body {
